ae28e527d3c2fe0422b5a3a144127099c4e9b998
Kadaverschreck <kadaverschreck@gmail.com>
2017-01-22 03:38:56 +0100

    Adding license and Console output

--- src/main/java/com/gmail/kadaverschreck/cbnamed/cbnamed.java
+++ src/main/java/com/gmail/kadaverschreck/cbnamed/cbnamed.java
@@ -1,3 +1,29 @@
+/*
+The MIT license (MIT)
+
+Copyright (c) 2017 Kadaverschreck
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This File is part of the cbnamed Plugin
+ */
+
 package com.gmail.kadaverschreck.cbnamed;
 
 
@@ -6,7 +32,6 @@ import org.spongepowered.api.Game;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandSpec;
 import org.spongepowered.api.event.Listener;
-import org.spongepowered.api.event.command.TabCompleteEvent;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.text.Text;
@@ -18,7 +43,8 @@ import java.util.logging.Logger;
  * Created by Kadaverschreck on 21.01.2017.
  */
 
-@Plugin(id="cbnamed", name="CbNamed", version="1.0")
+@Plugin(id="cbnamed", name="CbNamed", version="1.1", authors = "Kadaverschreck",
+        url = "https://github.com/Kadaverschreck/cbnamed")
 public class cbnamed {
 
     @Inject
--- src/main/java/com/gmail/kadaverschreck/cbnamed/cbnamedExecutor.java
+++ src/main/java/com/gmail/kadaverschreck/cbnamed/cbnamedExecutor.java
@@ -1,5 +1,32 @@
+/*
+The MIT license (MIT)
+
+Copyright (c) 2017 Kadaverschreck
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+This File is part of the cbnamed Plugin
+ */
+
 package com.gmail.kadaverschreck.cbnamed;
 
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
@@ -11,22 +38,15 @@ import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+import org.spongepowered.api.text.format.TextStyles;
 
 
 /**
  * Created by Kadaverschreck on 21.01.2017.
  */
+
 public class cbnamedExecutor implements CommandExecutor{
-
- /*   public ItemStack namedcb() {
-        String cbname = args.<String>getOne("cbname").get();
-        ItemStack namedcb = ItemStack.builder()
-                .itemType(ItemTypes.COMMAND_BLOCK).build();
-        namedcb.offer(Keys.DISPLAY_NAME, Text.of());
-        return namedcb;
-    }
-*/
-
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException{
@@ -36,6 +56,10 @@ public class cbnamedExecutor implements CommandExecutor{
                 .itemType(ItemTypes.COMMAND_BLOCK).build();
         namedcb.offer(Keys.DISPLAY_NAME, Text.of( cbname ));
         target.setItemInHand(HandTypes.OFF_HAND, namedcb);
+
+        //Gibt eine Meldung in der Konsole aus
+        Sponge.getServer().getConsole().sendMessage(Text.of(TextColors.WHITE, TextStyles.ITALIC, "Gave CommandBlock named ", cbname, " to " ,target.getName()));
+
         return CommandResult.success();
     }
 }
